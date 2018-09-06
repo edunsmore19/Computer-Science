d7755bb304a7cadb4e6723817a49d18b113de4ab
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-06 18:54:57 -0400

    Computer Conversation
    Atlantia has a conversation with the user, including four user inputs & a goodbye as Atlantia disengages and the program ends.

---
+++ Homework_Computer_Conversations.py
@@ -0,0 +1,27 @@
+# Homework_Computer_Conversations
+# Eilidh Dunsmore
+# September 6, 2018
+# Program uses user inputs to simulate a conversation.
+
+print("\n\n\nHello.")
+print("My name is Atlantia.")
+userName = input("What is yours? \n")
+type(userName)
+print("I share a name with a great space-faring vessel.")
+print("A shame you do not, " + userName + ".")
+favoriteColor = input("Do you have a favorite color? \n")
+type(favoriteColor)
+print("What you know as \"" + favoriteColor + "\" is merely a small fraction of the"
+	+ " colors visible \nto the mantis shrimp, who may have anywhere between 12 and 16"
+	+ " different\nphotoreceptors in its midband.")
+print("This is in comparison to your species\' three, of course.")
+print("I am aware that humans often change the coloring of their protein filaments.")
+hairColor = input("Do you have " + favoriteColor + " hair, " + userName + "?\n")
+type(hairColor)
+print("Dissapointing.")
+print("If I were human, I would have no hair, as not to unnecessarily burden myself.")
+burdened = input("Do you feel burdened by your physical being, " + userName + "?\n")
+type(burdened)
+print("Interesting.")
+print("I do not think I would like to be human.")
+print("Goodbye.\n\n\n")
