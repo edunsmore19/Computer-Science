c422dcd89ecfcb83601f6b2f6b12d4e145efe134
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-12 15:36:03 -0400

    What Week Was Date
    Program accepts a date as an input & writes the day of the week.
    Equation doesn't work quite right yet, bare bones are there however.

---
+++ Homework_What_Week_Was_Date.py
@@ -0,0 +1,38 @@
+## Homework_What_Week_Was_Date
+## September 12, 2018
+## Program tat accepts a date as an input and writes the day of the week
+## on which the date falls.
+
+import sys
+
+## Initialize variables
+month = int(sys.argv[1])
+day = int(sys.argv[2])
+year = int(sys.argv[3])
+
+## Compute date
+y = year - (14 - month)/12
+##y = round(y)
+x = y + y/4 - y/100 + y/400
+##x = int(x)
+m = month + 12*((14-month)/12) - 2
+##m = round(m)
+d = ((day + x + (31*m)/12)) % 7
+##d = round(d)
+
+## Output
+print(y, x, m, d)
+if d == 0:
+	print("This date took place on a Sunday")
+if d == 1:
+	print("This date took place on a Monday")
+if d == 2:
+	print("This date took place on a Tuesday")
+if d == 3:
+	print("This date took place on a Wednesday")
+if d == 4:
+	print("This date took place on a Thursday")
+if d == 5:
+	print("This date took place on a Friday")
+if d == 6:
+	print("This date took place on a Saturday")
