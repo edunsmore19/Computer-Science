8db301387b7026543bca6b6faf4e5f431db857e8
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-12 15:33:02 -0400

    Fahrenheit to Celcius
    Program accepts user input & converts it from Fahrenheit to Celcius

---
+++ Class_Work_Basic_Operations_Fahrenheit_to_Celcius.py
@@ -0,0 +1,7 @@
+## Class_Work_Basic_Operations_Fahrenheit_to_Celcius
+## September 12, 2018
+## Accepting user input & converting it as Fahrenheit to Celcius
+
+fahrenheit = float(input("Input Fahrenheit value: "))
+celcius = fahrenheit * 1.8 + 32
+print("In Celcius, this is ", celcius, " degrees.")
