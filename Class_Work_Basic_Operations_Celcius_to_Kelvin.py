17ff5f8484e03c99fdb7856e47f082ab4b7884a0
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-12 15:32:06 -0400

    Celcius to Kelvin
    Converting a variable in Celcius to Kelvin

---
+++ Class_Work_Basic_Operations_Celcius_to_Kelvin.py
@@ -0,0 +1,7 @@
+## Class_Work_Basic_Operations_Celcius_to_Kelvin
+## September 12, 2018
+## Converting a variable in Celcius to Kelvin
+
+celcius = float(input("Please enter degrees in Celcius: "))
+kelvin = celcius + 273.15
+print("Converted, the temperature in Kelvin is: ", kelvin)
