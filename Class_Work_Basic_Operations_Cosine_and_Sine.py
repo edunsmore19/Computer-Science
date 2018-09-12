67a2a88cfe1dd8a32127df845ae665bac0699e50
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-12 15:32:29 -0400

    Cosin and Sin Equation
    Computing a value wih cosine and sine

---
+++ Class_Work_Basic_Operations_Cosine_and_Sine.py
@@ -0,0 +1,7 @@
+## Class_Work_Basic_Operations_Cosine_and_Sine
+## September 12, 2018
+## Computing a value with cosine and sine
+
+theta = random.random() * m.pi * 2
+answer = m.sin2(theta)**2 + m.cos2(theta)**2
+print("Your answer is: ", answer)
