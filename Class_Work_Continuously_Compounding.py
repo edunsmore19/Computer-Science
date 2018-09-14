948ae6059b77a8803af51fa2c4969f09d1bba198
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-14 10:11:28 -0400

    Continuously Compound Program
    Added a new, functioning program which calculates & writes the compounded itnerest for variables.

---
+++ Class_Work_Continuously_Compounding.py
@@ -0,0 +1,18 @@
+## Class_Work_Continuously_Compounding
+## September 14, 2018
+## Program that calculates & writes the amount of money you'd have if you
+## invested it and it had continuously compounded interest.
+
+import sys
+import math
+
+## Initialize variables
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
+moneyTotal = 0
+
+## Compute equation
+moneyTotal = principal * (math.e ** (rate * time))
+moneyTotal = round(moneyTotal * 100) / 100
+print("Your money after", time, "years is:    ", moneyTotal)
