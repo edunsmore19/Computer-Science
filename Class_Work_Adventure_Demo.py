1d4e39c9f97b72415dd32fbb330427a08608ac0c
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-17 12:07:06 -0400

    Adventure Game Additions
    Now works.

--- Class_Work_Adventure_Demo.py
+++ Class_Work_Adventure_Demo.py
@@ -3,10 +3,10 @@
 ## Demo on a choose-your-own adventure style game
 
 def start():
-	response = input("Greetings! You are looking for treasure. Your map", 
-		"says that the treature can be found notherly--However, your friend",
-		"says that he heard someone talking about treasure in the south.",
-		"Do you 1) trust your friend, or 2) trust the map?")
+	response = input("""Greetings! You are looking for treasure. Your map
+says that the treature can be found notherly--However, your friend
+says that he heard someone talking about treasure in the south.
+Do you 1) trust your friend, or 2) trust the map?""")
 	if response == "1":
 		friend()
 	elif response == "2":
@@ -15,7 +15,21 @@ def start():
 		print("You must type 1 or 2. Please try again.")
 		start()
 def map():
-	pass
+	print("Congrats! You're now on your way to Treasure Island.")
+	congrats = input("""Now, to you take the highway, filled with
+banditmen? (Type 1) Or do you take the backroads, rumored to be
+filled with terrible beasts? (Type 2)""")
 def friend():
+	print("Your friend is a liar.")
+	print("You lose.")
+	response = lower(input("Want to try again?")) ## function 'lower' makes
+												  ## it NOT case sensitive
+	if response == "yes" or "Yes":
+		start()
+	else:
+		exit()
+def highway():
+	pass
+def backroads():
 	pass
 start()
