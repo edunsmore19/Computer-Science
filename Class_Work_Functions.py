142d51a967098126d7e5c10d3ec57dd1430ce776
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-09-17 11:42:01 -0400

    New Program Created
    Simple command work with functions

---
+++ Class_Work_Functions.py
@@ -0,0 +1,9 @@
+## Class_Work_Functions
+## September 17, 2018
+## Working with functions to perform various tasks.
+
+def greetings(something): ## Add a parameter to the function by putting
+						  ##something in the parentheses
+	print("Hello", something)
+
+greetings("Ted")
