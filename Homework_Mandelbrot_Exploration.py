afc5756c3f14ff4370d94d6217525456002150d9
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-10-18 20:05:21 -0400

    Everything Works Except PutPixel
    All the math & numbers bits are working but putPixel is being obstinate...

--- Homework_Mandelbrot_Exploration.py
+++ Homework_Mandelbrot_Exploration.py
@@ -12,15 +12,23 @@ green = 0
 global blue
 blue = 0
 
+global x
+x = 250
+global y
+y = 250
+
+global timesEscaped
+timesEscaped = 0
+
 z = [0, 0]
-c = [-2, 2]
+c = []
 zSquared = []
 zAndC = []
 tempC = [-2, 2]
 killerC = []
 
 ## Set size of image
-image = Image.new("RGB", (512, 512))
+image = Image.new("RGB", (500, 500))
 
 ## Generates a list of numbered pairs for 'c' to run through
 def generateNumbers():
@@ -28,9 +36,7 @@ def generateNumbers():
 	global itHasBegun
 	itHasBegun = False
 	for a in range(0, 3):
-		print("\n\na:", a)
 		for b in range(0, 3):
-			print("b:", b)
 			## Skips first pair b/c (-2,2) is already included in list
 			if (tempC[0] == (-2)) and (tempC[1] == 2) and (itHasBegun == False):
 				tempC.append(tempC[0] + 2)
@@ -47,88 +53,100 @@ def generateNumbers():
 			elif (b == 0) and (killerC[1] != 2):
 				tempC.append(killerC[0])
 				tempC.append(killerC[1])
-			print("tempC:", tempC)
-			print("killerC:", killerC)
 		killerC[0] = (-2)
 		killerC[1]-= 2
-
-
-
-## Changes 'c' and then rerouts to do calculation over again
-#def generateNumbers():
-	## Edits the x coordinate of 'c'
-#	for a in range(0, 3):
-#		print("\n\n\n\nCheck")
-		## Edits the y coordinate of 'c'
-#		for b in range(0, 3):
-#			print("2nd Check")
-#			print("c:", c)
-#			print("b:", b)
-#			if (c[0] == (-2)) and (c[1] == 2):
-#				tempY.append(tempY[0] - 2)
-#			if (b != 0):
-#				tempY.append(c[1] - 2)
-#			print("c:", c)
-#			print("Redirecting...\n\n")
-			#run()
-#		print("3rd Check")
-#		c[0] = c[0] + 2
-#		c[1] = 2
+	## Reroutes to 'run' in order to calculate equations
+	run()
 
 ## Calculate equations
 def run():
+	## If 'tempC' runs out of pairs, end program
+	if (len(tempC) == 0):
+		print("tempC is empty & you have no more pairs.")
+		exit()
 	## Equations
 	## Find z^2
 	zSquared.append(z[0]**2 - z[1]**2)
 	zSquared.append(2*z[0]*z[1])
-	print("ZSquared:", zSquared)
-	## Find z + c
+	print("\nZSquared:", zSquared)
+	print("tempC:", tempC)
+	## If 'z' has not ever escaped before, append first two values of 'tempC'
+	## to 'c' & then delete f/ 'tempC'
+	if (timesEscaped == 0):
+		c.append(tempC[0])
+		c.append(tempC[1])
+		del tempC[0:2]
+	print("c:", c)
+	## Now find z + c
 	zAndC.append(zSquared[0] + c[0])
 	zAndC.append(zSquared[1] + c[1])
-	del zSquared[0:2]
-	print("ZSquared:", zSquared)
 	print("zAndC:", zAndC)
 	check()
 
 ## Checks to see if z has escaped
 def check():
 	global timesEscaped
-	timesEscaped = 0
 	## Check if z escaped
 	if (((zAndC[0]**2 + zAndC[1]**2)**1/2) >= 2):
 		print("Value of Z:", ((zAndC[0]**2 + zAndC[1]**2)**1/2))
 		print("Z has escaped.")
+		## Clean up extra 'zAndC' values
 		del zAndC[0:2]
 		print("zAndC:", zAndC)
-		if (timesEscaped == 0):
-			red = 255
-			green = 0
-			blue = 0
-		elif (timesEscaped == 1):
-			red = 0
-			green = 255
-			blue = 0
-		elif (timesEscaped == 2):
-			red = 0
-			green = 0
-			blue = 255
-		## Draw pixel
-		print("draw pixel")
-		image.putpixel((z[0], z[1]), (red, green, blue))
+		## Clean up extra 'zSquared' values
+		del zSquared[0:]
+		print("zSquared:", zSquared)
+		## Reroute to 'drawPixel'
+		drawPixel()
 	else:
 		timesEscaped+= 1
 		print("timesEscaped:", timesEscaped)
-	if (timesEscaped >= 3):
-		print("timesEscaped has reached its limit")
-		exit()
-	## Redirect to newNumber for a new set of coordinates
-	newNumber()
+		## Clean up extra 'zAndC' values
+		del zAndC[0:2]
+		print("zAndC:", zAndC)
+		## Clean up extra 'zSquared' values
+		del zSquared[0:]
+		print("zSquared:", zSquared)
+		## Kill-command for if 'z' escapes more than 3 times
+		if (timesEscaped >= 3):
+			print("timesEscaped has reached its limit")
+			## Redirects to 'drawPixel'
+			drawPixel()
+		run()
+
+## WILL NOT DRAW PIXEL
+## Draws pixel
+def drawPixel():
+	global timesEscaped
+	## Give 'c' coordinates real life x/y counterparts
+	x = 250
+	x+= c[0]
+	y = 250
+	y+= c[1]
+	del c[0:2]
+	print("x:", x, "y:", y)
+	## Determine color
+	if (timesEscaped == 0):
+		red = 255
+		green = 0
+		blue = 0
+	elif (timesEscaped == 1):
+		red = 0
+		green = 255
+		blue = 0
+	elif (timesEscaped == 2):
+		red = 0
+		green = 0
+		blue = 255
+	## Print pixel
+	image.putpixel((x, y), (255, 0, 0))
+	## Reset 'timesEscaped' to present as 0 again
+	timesEscaped = 0
+	## Redirect back to 'run'
+	run()
 
 ## Save name/file type
 image.save("Homework_Mandelbrot_Exploration.png", "PNG")
 
-## Test print
-print("z:", z)
-print("c:", c)
 ## Run program
 generateNumbers()
