cb296bbdf7ab40358a4e09fdee0903aa324ab8af
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-11-06 22:35:23 -0500

    More Story Added, But Trouble Ahead
    Upside: more story added
    Downside: can't get 'choice' variable to refresh. cry.

--- Project_Fall_Final.py
+++ Project_Fall_Final.py
@@ -71,27 +71,95 @@ def roomOne():
 
 ## Outcomes for choices made in 'roomOne'
 def roomOneChoices():
+	global thing
+	global action
+	## Prints error message if there is no known 'thing' being interacted with
+	unknownThing()
 	if (thing == "window"):
-		if (action == "talk to") or (action == "give") or (action == "show") or (action == "take")
-		or (action == "buy") or (action == "eat") or (action == "drink") or (action == "drop")
-		or (action == "turn"):
-			print("\nWhy would you want to do that?")
-			## Prompt user to input new commands that make more sence
-			global choice
+		## Prints error message if there is no known 'action' being done
+		unknownAction()
+		if (action == "talk to") or (action == "give") or (action == "show") or (action == "take") or (action == "buy") or (action == "eat") or (action == "drink"):
+			why()
+		## Print approach line
+		else:
+			print("\nYou approach the boarded up window.")
+		## Valid story choices
+		if (action == "look at"):
+			print("The wood is old and dry, and you can just barely peer through. You realize " +
+			"you are on the second floor of a building. Outside, you see a barren, decaying " +
+			"cityscape. The streets are empty of people and animals.")
+			print("As you stare at the skeleton of what might have long ago been a skyscraper, you " +
+			"see movement out of the corner of your eye. There is a man stumbling out of an alley " +
+			"into the street. He moves slowly, dragging his bad leg behind him seemingly without " +
+			"care or any indication of pain. You watch him continue to amble down the street until " +
+			"he is only a couple of yards away from the building you're in. It's then that you notice " +
+			"the bluish-green tinge of his flesh and the several rotting, open wounds covering his " +
+			"body.")
+			print("You realize: He's a zombie.")
+			print("You retreat from the window.")
+			## Prompt user to continue exploring the room
 			choice = input()
 			parse()
-		elif (action == "look at"):
 		elif (action == "open"):
+			print("You realize that the window has no glass in it and that the wooden boards " +
+			"nailed into frame are the only thing between you and the street two stories below.")
+			print("You decide you no longer want to open the window.")
+			choice = input()
+			parse()
 		elif (action == "push"):
+			print("You realize that the window has no glass in it and that the wooden boards " +
+			"nailed into frame are the only thing between you and the street two stories below.")
+			print("You decide you no longer want to try pushing on the boards covering the window.")
 		elif (action == "feel"):
-		elif (action == "unknown"):
+			print("You run your hand over the dry, old wood covering the window.")
+			print("You discover the wood has splinters.")
+			choice = input()
+			parse()
+
+	elif (thing == "backpack"):
+		unknownAction()
+		if (action == "talk to") or (action == "give") or (action == "show") or (action == "buy") or (action == "eat") or (action == "drink"):
+			why()
+		## Print approach line
+		else:
+			print("\nYou approach the backpack.")
+		## Valid story choices
+		if (action == "look at"):
+			print("You look at the slightly worn backpack. It's a navy blue color and has your name, " +
+			player.name + " stitched into the back in uneven grey thread. It looks usable, but empty.")
+			print("Maybe it woulf be a good idea to take it?")
+		elif (action == "take"):
+			print("You pick the backpack up off the ground and swing it onto one shoulder. It feels " +
+			"comfortable, and empty.")
+		elif (action == "open"):
+			print("You kneel down and unzip the backpack carefully. You peer inside and see that it " +
+			"is empty.")
+			print("Maybe you could use it to carry things?")
+		elif (action == "push"):
+			print("You nudge the backpack with your toe.")
+			print("It shifts several inches.")
+		elif (action == "feel"):
+			print("You kneel down and run a hand over the tough navy canvas backpack. It feels, " +
+			"rough to the touch, but strong. You ghost your fingers over your name, " + player.name +
+			" stitched into the canvas. The thread is slightly worn and was probably once white but " +
+			"it's now turned a greyish color.")
+			print("The backpack is empty right now, but it could carry a lot you surmise.")
 
 
 
-	#elif (thing == "door")
-	#elif (thing == "dresser")
-	#elif (thing == "backpack")
-	#elif (thing == "unknown")
+
+
+
+
+	#elif (thing == "dresser"):
+
+#	elif (thing == "door"):
+#		unknownAction()
+#		if (action == "talk to") or (action == "give") or (action == "show") or (action == "take") or (action == "buy") or (action == "eat") or (action == "drink"):
+#			why()
+#		## Print approach line
+#	else:
+#		print("\nYou approach ")
 
 ## Cut up & parse meaning from user input
 def parse():
@@ -109,14 +177,10 @@ def parse():
 		action = "look at"
 	elif (choice.find("take") != (-1)):
 		action = "take"
-	elif (choice.find("drop") != (-1)):
-		action = "drop"
 	elif (choice.find("open") != (-1)):
 		action = "open"
 	elif (choice.find("push") != (-1)):
 		action = "push"
-	elif (choice.find("turn") != (-1)):
-		action = "turn"
 	elif (choice.find("feel") != (-1)):
 		action = "feel"
 	elif (choice.find("eat") != (-1)):
@@ -157,6 +221,40 @@ def parse():
 	if (roomOneCompleted == False):
 		roomOneChoices()
 
+## Error message for when the user inputs an action that is unknown
+def unknownAction():
+	if (action == "unknown"):
+		print("\nYou don't know how to do that.")
+		print("Try commands like: 'talk to', 'give to', 'show to', 'look at', 'take', " +
+		"'open', 'push', 'feel', 'buy', 'eat', or 'drink'.")
+		global choice
+		choice = input()
+		parse()
+
+## Error message for when the user inputs a thing that is unknown
+def unknownThing():
+	if (thing == "unknown"):
+		print("\nYou don't see or can't interact with that object.")
+		print("Try an object that is highlighted in the room description.")
+		global choice
+		choice = input()
+		parse()
+
+## Error message for when the user inputs a person that is unknown
+def unknownPerson():
+	if (thing == "unknown"):
+		print("\nWho is that person?")
+		print("Try someone whose name is highlighted.")
+		global choice
+		choice = input()
+		parse()
+## Error message for when the user is trying to do weird things
+def why():
+	print("\nWhy would you want to do that?")
+	## Prompt user to input new commands that make more sense
+	global choice
+	choice = input()
+	parse()
 
 ## Begin program
 welcomeScreen()
