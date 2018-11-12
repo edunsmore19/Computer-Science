b11d92bf91eedcdcfaf36f894656c35170163068
Eilidh Dunsmore <edunsmore19@choate.edu>
2018-11-12 11:57:17 -0500

    Filter Now Works, but Not Very Cool

--- In_Class_Photo_Filter.py
+++ In_Class_Photo_Filter.py
@@ -1,3 +1,38 @@
 ## In_Class_Photo_Filter
+## Honor Code: I have neither given nor recieved any unauthorized aid.
+## Sources:
+## Figuring out how to load a given image & find pixel values in rgb for an already
+## rendered image.
+## https://stackoverflow.com/questions/138250/how-can-i-read-the-rgb-value-of-a-given-pixel-in-python
 ## November 12, 2018
 ## Add a filter/effect to a photo of the user's choosing.
+
+import sys
+import colorsys
+from PIL import Image
+
+## Initialize variables
+imageName = sys.argv[1]
+image = Image.open(imageName)
+width, height = image.size
+
+## Show the image!
+image.show()
+
+## Muckin' about
+for y in range(height):
+	for x in range(width):
+		colors = image.getpixel((x, y))
+		r, g, b = colors
+		r = r - 50
+		g = g - 50
+		b = b - 50
+		image.putpixel((x, y), (r, g, b))
+
+image.show()
+
+
+print(imageName)
+print(image)
+print(width)
+print(height)
