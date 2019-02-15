9a4d8e49ef3b2a22b4007d99510c50d478a07729
Eilidh Dunsmore <edunsmore19@choate.edu>
2019-02-15 14:18:09 -0500

    Fixed Haiku
    Fixed the thing that was throwing errors & constructed a new text for the haikus to draw from & haikus are now executing correctly.

--- Project_Poetry_Generator_Final.py
+++ Project_Poetry_Generator_Final.py
@@ -7,10 +7,14 @@
 ## Import libraries
 import nltk
 import random
+import string
+from nltk.corpus import cmudict
+cmu = cmudict.dict()
 
 ## Importing my emo poetry as the corpus
 global poetry
 poetry = open("Eilidh's Work 17, 18, 19.txt")
+#poetry = open("Haiku Text.txt")
 
 ## Tokenize 
 from nltk.tokenize import word_tokenize as word
@@ -30,6 +34,12 @@ global numLines
 numLines = random.randint(3, 11)
 global numLinesCounter
 numLinesCounter = 0
+global line
+line = 0
+global haikuWords
+haikuWords = []
+global haikuLineNum
+haikuLineNum = 0
 
 ## This will generate poems based on cfd
 def cfdPoetryGeneration():
@@ -77,5 +87,82 @@ def cleanText():
 	if (numLinesCounter != numLines):
 		cfdPoetryGeneration()
 
+## This rhyming component will count syllables
+def countSyllables(word):
+	lowerWord = word.lower()
+	if lowerWord in cmu:
+		## If the word is in 'cmu' then return the number of syllables
+		return max([len([y for y in x if y[-1] in string.digits]) for x in cmu[lowerWord]])
+
+## This will generate haikus
+def haikuMe():
+	## Generate a random seed word
+	randomWord = random.choice(tokenizedPoetry)
+	global haikuWords
+	global line
+	global syllableCount
+	syllableCount = countSyllables(randomWord)
+	#print("syllableCount", syllableCount)
+	## If 'syllableCount' is a number & not a space, wrap as an 'int'
+	if (syllableCount == 1) or (syllableCount == 2) or (syllableCount == 3) or (syllableCount == 4) or (syllableCount == 5) or (syllableCount == 6) or (syllableCount == 7):
+		syllableCount = int(syllableCount)
+	## If it's a space, redirect to 'haikuMe' to generate a new word
+	else:
+		haikuMe()
+
+	## If 'haikuLineNum' is 0 or 2, 5 syllables
+	if (haikuLineNum == 0) or (haikuLineNum == 2):
+		## If the word generated is greater than five syllables, re-generate
+		if ((line + syllableCount) > 5):
+			haikuMe()
+		## If the word generated is less than five syllables, add to line &
+		## generate a new word
+		elif ((line + syllableCount) < 5):
+			haikuWords.append(randomWord)
+			line+= syllableCount
+			haikuMe()
+		## If the word generated is exactly five syllables move onto the
+		## next line
+		elif ((line + syllableCount) == 5):
+			haikuWords.append(randomWord)
+			##Reset 'line' to zero
+			line = 0
+			## Redirect to 'printHaiku'
+			printHaiku()
+	## If 'haikuLineNum' is 1, 7 syllables
+	elif (haikuLineNum == 1):
+		## If the word generated is greater than seven syllables, re-generate
+		if ((line + syllableCount) > 7):
+			haikuMe()
+		## If the word generated is less than seven syllables, add to line &
+		## generate a new word
+		elif ((line + syllableCount) < 7):
+			haikuWords.append(randomWord)
+			line+= syllableCount
+			haikuMe()
+		## If the word generated is exactly seven syllables move onto the
+		## next line
+		elif ((line + syllableCount) == 7):
+			haikuWords.append(randomWord)
+			##Reset 'line' to zero
+			line = 0
+			## Redirect to 'printHaiku'
+			printHaiku()
+	else:
+		exit()
+
+## This will print the haiku
+def printHaiku():
+	global haikuWords
+	print(*haikuWords)
+	## Empty 'haikuWords' list
+	haikuWords = []
+	## Add one to 'haikuLineNum' counter in order to move onto next line
+	global haikuLineNum
+	haikuLineNum+= 1
+	## Redirect to 'haikuMe'
+	haikuMe()
+
 ## Begin generating poetry & redirect to 'cfdPoetryGeneration'
 cfdPoetryGeneration()
+#haikuMe()
